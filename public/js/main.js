f6a2d93edee5e77d90e54d0e34ec05fa1cae3115
Luka Matovic <lukamatovic1994@gmail.com>
2023-11-23 23:52:48 -0600

    Adding more of CSS

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
 const addButton = document.querySelector(".add-task-btn");
 const addInput = document.querySelector(".new-task-input");
 
+
 document.addEventListener("DOMContentLoaded", () => {
-  addButton.addEventListener("click", () => {
+  
+    addButton.addEventListener("click", () => {
     setTimeout(() => {
       addInput.value = "";
     }, 500);
   });
+
+  
+
+
 });
